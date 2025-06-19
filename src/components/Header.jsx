3f23955a3debf71da53eb69df56ee88393969c7c
itsmesoham <sohampoptani7@gmail.com>
2025-06-19 19:26:29 +0530

    commit #home error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
 
                 <h1>
-                    <a href="/" className="logo">
+                    <a href="#home" className="logo">
                         <img
                             src={logo}
                             width={40}
