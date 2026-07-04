98b74d0a1a732b0d3348d992515b05db7a543669
itsmesoham <sohampoptani7@gmail.com>
2026-07-04 11:04:36 +0530

    resume-changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 
 import { ButtonPrimary, ButtonOutline } from "./Button"
 import avatar from '../assets/images/avatar-2.jpg'
-import resume from '../../public/Resume_Soham-Poptani.pdf'
+import resume from '../../dist/Soham_Poptani - Resume.pdf'
 import photo from '../assets/images/myphoto2.png'
 
 const Hero = () => {
